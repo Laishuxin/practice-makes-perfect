12feb9a435000448f06bd6a946150542ab2b38d6
Laishuxin <56504759+Laishuxin@users.noreply.github.com>
2021-10-29 15:13:20 +0800

    feat(venilla): looseEqual and shallowEqual

--- src/venilla/1-javascript-fundation/11-shallow-equal/index.js
+++ src/venilla/1-javascript-fundation/11-shallow-equal/index.js
@@ -1,1 +1,38 @@
-export function shallowEqual() {}
+const hasOwnProperty = Object.prototype.hasOwnProperty
+/**
+ * `shouldComponentUpdate` 底层实现判断原理。
+ * @param {any} o1
+ * @param {any} o2
+ * @returns { boolean }
+ */
+export function shallowEqual(o1, o2) {
+  if (o1 === o2) {
+    return true
+  }
+
+  if (
+    typeof o1 !== 'object' ||
+    typeof o2 !== 'object' ||
+    o1 === null ||
+    o2 === null
+  ) {
+    return false
+  }
+
+  const keys1 = Object.keys(o1)
+  const keys2 = Object.keys(o2)
+  if (keys1.length !== keys2.length) {
+    return false
+  }
+
+  for (let i = 0; i < keys1.length; i++) {
+    if (
+      !hasOwnProperty.call(o2, keys1[i]) ||
+      !Object.is(o1[keys1[i]], o2[keys1[i]])
+    ) {
+      return false
+    }
+  }
+
+  return true
+}
