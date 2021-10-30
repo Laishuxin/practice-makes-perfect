0dade2e2dcf289d887b063ef0e4985ef5b1fdc51
Laishuxin <56504759+Laishuxin@users.noreply.github.com>
2021-10-30 16:31:55 +0800

    feat(venilla): debounce and throttle

--- src/venilla/1-javascript-fundation/4-debounce/index.js
+++ src/venilla/1-javascript-fundation/4-debounce/index.js
@@ -1,1 +1,7 @@
-export function debounce() {}
+/**
+ * @param { Function } fn
+ * @param {{wait?: number, trailing?: boolean, leading?: boolean}} options
+ * @default {wait=500}
+ * @returns { Function } debouncedFn
+ */
+export function debounce(fn, options = {}) {}
--- src/venilla/1-javascript-fundation/5-throttle/index.js
+++ src/venilla/1-javascript-fundation/5-throttle/index.js
@@ -1,1 +1,7 @@
-export function throttle() {}
+/**
+ * @param { Function } fn
+ * @param {{wait?: number, trailing?: boolean, leading?: boolean}} options
+ * @default {wait=500}
+ * @returns { Function } throttledFn
+ */
+export function throttle(fn, options = {}) {}
