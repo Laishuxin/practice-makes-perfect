c0845929c6c53e530ebc1d03daf9af3ced431856
Laishuxin <56504759+Laishuxin@users.noreply.github.com>
2021-11-08 09:51:00 +0800

    feat(venilla): apply

--- src/venilla/1-javascript-fundation/7-apply/index.js
+++ src/venilla/1-javascript-fundation/7-apply/index.js
@@ -1,2 +1,19 @@
-export const MyFunction = Function
-MyFunction.apply1 = function () {}
+import { window } from '../shared/util'
+function arrayLike(a) {
+  return a && a.length !== void 0
+}
+/**
+ * @param {*} context
+ * @param {any[]} args
+ */
+Function.prototype.apply1 = function (context, args) {
+  if (args && !arrayLike(args)) {
+    throw new TypeError(`CreateListFromArrayLike called on non-object`)
+  }
+  context = context != void 0 ? Object(context) : window
+  const fn = Symbol()
+  context[fn] = this
+  const result = args === void 0 ? context[fn]() : context[fn](...args)
+  delete context[fn]
+  return result
+}
